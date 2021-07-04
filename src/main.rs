b1ca932780c021acd46063e09b8c0a08eac6334f
Joshua T. Fisher <playmer@gmail.com>
2021-07-03 23:07:45 -0400

    trying to get the parser to work

--- src/main.rs
+++ src/main.rs
@@ -53,11 +53,9 @@ impl Widget<u32> for TimerWidget {
 
 use std::time::Duration;
 
+use druid::piet::Text;
 use druid::widget::{Button, Checkbox, DisabledIf, Flex, Label, LensWrap, Parse, TextBox};
-use druid::{
-    AppLauncher, Data, Env, Event, EventCtx, FontDescriptor, FontFamily, Lens, LocalizedString, TimerToken, UnitPoint, Widget,
-    WidgetExt, WindowDesc, WindowConfig
-};
+use druid::{AppLauncher, Data, Env, Event, EventCtx, FontDescriptor, FontFamily, Lens, LensExt, LocalizedString, TimerToken, UnitPoint, Widget, WidgetExt, WindowConfig, WindowDesc};
 
 use druid_shell::{WindowLevel};
 
@@ -67,7 +65,7 @@ const WINDOW_TITLE: LocalizedString<SaveReminderState> = LocalizedString::new("H
 
 #[derive(Clone, Data, Lens)]
 struct SaveReminderState {
-    minutes_to_wait: String,
+    minutes_to_wait: u64,
     repeat: bool,
     timer_started : bool
 }
@@ -80,7 +78,7 @@ pub fn main() {
 
     // create the initial app state
     let initial_state = SaveReminderState {
-        minutes_to_wait: "5".into(),
+        minutes_to_wait: 5,
         repeat: true,
         timer_started: false
     };
@@ -113,14 +111,27 @@ fn build_root_widget() -> impl Widget<SaveReminderState> {
     .align_horizontal(UnitPoint::CENTER);
 
     let mut input_row = Flex::row();
+
+    //let parser = Parse::new(TextBox::new());
+    //input_row.add_child();
+
+
     input_row.add_child(Label::new("Remind in X Minutes:"));
     // a textbox that modifies `minutes_to_wait`.
-    input_row.add_child(TextBox::new()
+    let textbox = TextBox::new()
         .with_placeholder("5")
-        .with_text_size(18.0)
-        .fix_width(TEXT_BOX_WIDTH)
-        .align_horizontal(UnitPoint::CENTER)
-        .lens(SaveReminderState::minutes_to_wait));
+        .with_text_size(18.0);
+        
+    let textbox = LensWrap::new(
+        Parse::new(TextBox::new()),
+        SaveReminderState::minutes_to_wait.map(|x| {
+            String::from("")
+        }, |x, y| {
+            *x = y.parse::<u64>().unwrap();
+        })
+    );
+
+    input_row.add_child(textbox);
         
     let mut repeat_row = Flex::row();
     repeat_row.add_child(Label::new("Should Repeat Reminder:"));
@@ -129,44 +140,45 @@ fn build_root_widget() -> impl Widget<SaveReminderState> {
     let mut button_row = Flex::row();
 
     let start_button = Button::new("Start").on_click(|context, data: &mut SaveReminderState, env| {
-        match data.minutes_to_wait.parse::<u64>() {
-            Ok(minutes_to_wait_num) => {
-                println!("{}", minutes_to_wait_num);
-                //let mut timer = context.request_timer(Duration::from_secs(minutes_to_wait_num * 60));
-                data.timer_started = true;
-            }
-            Err(_e) => {
-                data.minutes_to_wait = String::from("5");
-                context.new_sub_window(
-                    WindowConfig::default()
-                        .show_titlebar(false)
-                        .set_level(WindowLevel::Modal)
-                        .window_size((240.0,100.0))
-                        .set_position(
-                            context.window().get_position() + (100.0, 100.0)),
-                    build_modal_error_widget(),
-                    data.clone(),
-                    env.clone(),
-                );
-            },
-        }
+        data.timer_started = true;
+        //match data.minutes_to_wait.parse::<u64>() {
+        //    Ok(minutes_to_wait_num) => {
+        //        println!("{}", minutes_to_wait_num);
+        //        //let mut timer = context.request_timer(Duration::from_secs(minutes_to_wait_num * 60));
+        //        data.timer_started = true;
+        //    }
+        //    Err(_e) => {
+        //        data.minutes_to_wait = 5;
+        //        context.new_sub_window(
+        //            WindowConfig::default()
+        //                .show_titlebar(false)
+        //                .set_level(WindowLevel::Modal)
+        //                .window_size((240.0,100.0))
+        //                .set_position(
+        //                    context.window().get_position() + (100.0, 100.0)),
+        //            build_modal_error_widget(),
+        //            data.clone(),
+        //            env.clone(),
+        //        );
+        //    },
+        //}
     });
 
-    let potentially_disabled_start_button = DisabledIf::new(start_button, |data, env| {
+    let start_button = DisabledIf::new(start_button, |data, env| {
         data.timer_started
     });
 
-    button_row.add_child(potentially_disabled_start_button);
+    button_row.add_child(start_button);
 
     let stop_button = Button::new("Stop").on_click(|context, data: &mut SaveReminderState, env|{
         data.timer_started = false;
     });
 
-    let potentially_disabled_stop_button = DisabledIf::new(stop_button, |data, env| {
+    let stop_button = DisabledIf::new(stop_button, |data, env| {
         !data.timer_started
     });
 
-    button_row.add_child(potentially_disabled_stop_button);
+    button_row.add_child(stop_button);
 
     // arrange the two widgets vertically, with some padding
     Flex::column()
